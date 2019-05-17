9a1ff1534078dc5af9131939d8a489734c7be9fb
gerardoadame <gera_skate@outlook.com>
2019-05-17 12:55:39 -0500

    version intento

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ditel Prro no sale 
+                    Ditel
                 </div>
 
                 <div class="links">
