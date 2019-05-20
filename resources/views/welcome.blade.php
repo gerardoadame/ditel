4f358f0b3f9b25add3030507777103b49c291e3a
gerardoadame <gera_skate@outlook.com>
2019-05-20 12:24:29 -0500

    probando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Ditel
+                    Bienvenido a Ditel
                 </div>
 
                 <div class="links">
